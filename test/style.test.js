4293d22ea25606f161931904f4cc04da6596da4b
Rafal Chlodnicki <rchl2k@gmail.com>
2023-04-16 00:38:29 +0200

    Update to stylelint 15

--- test/style.test.js
+++ test/style.test.js
@@ -5,13 +5,15 @@ test('that errors are reported in scss', () => {
 
     expect(execResult.status).not.toBe(0);
 
+    /** @type {import('stylelint').LintResult[]} */
     const issues = JSON.parse(execResult.stdout);
 
     expect(issues.length).toBe(1);
-    expect(issues[0].warnings.length).toBe(3);
-    expect(issues[0].warnings[0].rule).toBe('declaration-colon-space-after');
-    expect(issues[0].warnings[1].rule).toBe('function-name-case');
-    expect(issues[0].warnings[2].rule).toBe('rule-empty-line-before');
+    expect(issues[0].warnings.map(warning => warning.rule)).toEqual([
+        'declaration-colon-space-after',
+        'function-name-case',
+        'rule-empty-line-before',
+    ]);
 });
 
 test('that errors are reported in vue', () => {
@@ -19,11 +21,13 @@ test('that errors are reported in vue', () => {
 
     expect(execResult.status).not.toBe(0);
 
+    /** @type {import('stylelint').LintResult[]} */
     const issues = JSON.parse(execResult.stdout);
 
     expect(issues.length).toBe(1);
-    expect(issues[0].warnings.length).toBe(3);
-    expect(issues[0].warnings[0].rule).toBe('color-no-invalid-hex');
-    expect(issues[0].warnings[1].rule).toBe('declaration-block-trailing-semicolon');
-    expect(issues[0].warnings[2].rule).toBe('selector-pseudo-element-no-unknown');
+    expect(issues[0].warnings.map(warning => warning.rule)).toEqual([
+        'color-no-invalid-hex',
+        'declaration-block-trailing-semicolon',
+        'selector-pseudo-element-no-unknown',
+    ]);
 });
